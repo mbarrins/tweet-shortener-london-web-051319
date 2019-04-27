a06a16563951d0b01223a2e37e4b4c433407fb2e
Maija Barrins <maija.barrins@gmail.com>
2019-04-27 21:28:12 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split(" ").map {|word| dictionary.include?(word.downcase) ? dictionary[word.downcase] : word}.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each{|tweet| puts word_substituter(tweet)}
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] << "..."
+  else
+    selective_tweet_shortener(tweet)
+  end
+end
