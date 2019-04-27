394f0b040034e7d31e4b2bf1a8b42520ce037471
Maija Barrins <maija.barrins@gmail.com>
2019-04-27 21:17:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,7 +24,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if selective_tweet_shortener(tweet).length > 140
-    selective_tweet_shortener(tweet)
+    selective_tweet_shortener(tweet)[0..137] << "..."
   else
     selective_tweet_shortener(tweet).length
   end
