d46ee5f296d3d2d24644719ab8b7b204e4d1a239
Maija Barrins <maija.barrins@gmail.com>
2019-04-27 20:59:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def dictionary
+  {
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter
+
+end
